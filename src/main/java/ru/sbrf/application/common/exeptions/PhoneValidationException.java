d199eb60598dfc0dfcde6e9da41a716c3ac88511
Miah4rde <awlen@mail.ru>
2020-09-13 17:29:57 +0300

    Application payment

--- src/main/java/ru/sbrf/application/common/exeptions/PhoneValidationException.java
+++ src/main/java/ru/sbrf/application/common/exeptions/PhoneValidationException.java
@@ -1,8 +1,34 @@
 package ru.sbrf.application.common.exeptions;
 
-public class PhoneValidationException {
+public class PhoneValidationException extends RuntimeException {
 
     private String phone;
 
+    public PhoneValidationException(String phone) {
+        this.phone = phone;
+    }
 
+    public PhoneValidationException(String message, String phone) {
+        super(message);
+        this.phone = phone;
+    }
+
+    public PhoneValidationException(String message, Throwable cause, String phone) {
+        super(message, cause);
+        this.phone = phone;
+    }
+
+    public PhoneValidationException(Throwable cause, String phone) {
+        super(cause);
+        this.phone = phone;
+    }
+
+    public PhoneValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, String phone) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
